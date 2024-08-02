2cad6938f049373b56e3d581482b18ce5110f4bd
Hasky <Walker_ACR_ITSec@outlook.com>
2024-08-02 02:38:35 +0000

    Arskell push

---
+++ Arskell.sh
@@ -0,0 +1,273 @@
+#!/usr/bin/env bash
+
+# ---------------------------------------------------------------
+# Arskell Installer (Haskell Arch)                               |
+# ---------------------------------------------------------------
+# Author    : Binary-Brawler                                     |
+# Github    : github.com/Binary-Brawler                          |
+# LinkedIn  : linkedin.com/in/brandon-walker-0b0542116/          |
+# Version   : 1.0.0                                              |
+# Intent    : Simplistic Linux box for Haskell Development       | 
+# ---------------------------------------------------------------
+
+# Constants
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+WHITE='\033[0;37m'
+YELLOW='\033[0;33m'
+PURPLE='\033[0;35m'
+RESET='\033[0m'
+NEWLINE=$'\n'
+GITHUB='https://raw.githubusercontent.com/Binary-Brawler/Arskell'
+
+# Logging
+log_warning() { echo -e "[${YELLOW}WARNING${RESET}] $1"; }
+log_error() { echo -e "[${RED}ERROR${RESET}] $1"; }
+print_info() { echo -e "[${GREEN}INFO${RESET}] $1${NEWLINE}"; }
+log_success() { echo -e "[SUCCESS] $1"; }
+
+# Repeats turned into functions ;) 
+sleep_and_clear() {
+    sleep 3
+    clear
+}
+
+# User Input
+prompt_for_input() {
+    read -p "$1" response
+    echo "$response"
+}
+
+# Simple greeter function
+greet() {
+    sleep_and_clear
+	echo -e "${PURPLE}Binary-Brawler's Haskell Arch Installer${RESET}"
+	echo ${NEWLINE}
+	echo ${NEWLINE}
+    echo "-------------------------------------------------------------------------------------------------"
+	echo -e ${YELLOW}
+	echo ${NEWLINE}
+	echo ${NEWLINE}
+    printf " _    _           _        _ _                     _     \n"
+    printf "| |  | |         | |      | | |     /\\            | |    \n"
+    printf "| |__| | __ _ ___| | _____| | |    /  \\   _ __ ___| |__  \n"
+    printf "|  __  |/ _\` / __| |/ / _ \\ | |   / /\\ \\ | '__/ __| '_ \\ \n"
+    printf "| |  | | (_| \\__ \\   <  __/ | |  / ____ \\| | | (__| | | |\n"
+    printf "|_|  |_\\__,_|___/_|\\_\\___|_|_| /_/    \\_\\_|  \\___|_| |_|"
+    echo ${NEWLINE}                                                    
+	echo -e "${RESET}"
+	echo ${NEWLINE}
+	echo ${NEWLINE}
+    echo "-------------------------------------------------------------------------------------------------"
+}
+
+# Check Internet Connection
+checkNet() {
+    echo "---------------------------------------"
+    print_info "Checking Internet Connection..."
+    
+    # Check if speedtest-cli is installed
+    if ! command -v speedtest-cli &>/dev/null; then
+        print_info "speedtest-cli is not installed. Installing..."
+        pacman -Sy >/dev/null
+        python3 -m venv speedTest && cd speedTest/bin/ && ./pip3 install speedtest-cli >/dev/null
+    fi
+    
+    # Perform speed test
+    print_info "Calculating Internet Speed..."
+    sleep 1
+    local speed=$(./speedtest-cli --simple | awk '/^Download:/ {print $2}')
+    sleep 1
+    echo "------------------------------------"
+    print_info "Download Speed: $speed Mbps"
+    echo "------------------------------------"
+    
+    # Check if download speed is greater than 50 Mbps
+    if (( $(echo "$speed > 50" | bc -l) )); then
+        print_info "Enabling Parallel Downloads..."
+        sed -i '/#ParallelDownloads/s/^#//g' /etc/pacman.conf
+        echo "true" > enable_parallel.txt
+    else
+        print_info "Keeping Parallel Downloads Disabled as speed is too slow."
+        echo "false" > enable_parallel.txt
+    fi
+}
+
+# Calculate Swap Size Based on System RAM
+# Improve this...
+calculate_swap_size() {
+    local ram_gb=$(free -g | awk '/^Mem:/{print $2}')
+    local swap_size="4G" # Default
+
+    if [[ ! $ram_gb =~ ^[0-9]+$ ]]; then
+        log_error "Error: Could not determine RAM size." >&2
+        exit 1
+    fi
+
+    if ((ram_gb < 2)); then
+        log_error "Error: Insufficient RAM (< 2GB)." >&2
+        exit 1
+    elif ((ram_gb < 4)); then
+        swap_size="${ram_gb}G"
+    elif ((ram_gb < 16)); then
+        swap_size="4G"
+    else
+        swap_size="8G"
+    fi
+
+    echo "$swap_size"
+}
+
+# Partitioning Function
+partition_drive() {
+    local drive="$1"
+    local swap_size=$(calculate_swap_size)
+    local cmd_list=()
+
+    if [[ -d "/sys/firmware/efi/efivars/" ]]; then
+        cmd_list=(
+            "g"                          # Create a new GPT table
+            "n" "" "" "+512M"            # EFI partition
+            "n" "" "" "+$swap_size"      # Swap partition
+            "n" "" "" ""                 # Root partition
+            "t" "1" "1"                  # Set type of EFI partition to EFI System
+            "t" "2" "19"                 # Set type of swap to Linux swap
+            "w"                          # Write changes
+        )
+
+    else
+        cmd_list=(
+            "o" "n" "p" "" "" "+$swap_size"   # Swap partition
+            "n" "p" "" "" ""                  # Root partition
+            "t" "1" "82"                      # Set type of first partition to Linux swap
+            "w"                               # Write changes
+        )
+
+    fi
+
+    # Run fdisk commands
+    printf "%s\n" "${cmd_list[@]}" | fdisk "$drive" >/dev/null 2>&1
+
+    # Check for fdisk errors
+    if [[ $? -ne 0 ]]; then
+        echo "Error: Failed to partition the drive."
+        return 1
+    fi
+
+    # Format partitions and mount them
+    if [[ -d "/sys/firmware/efi/efivars/" ]]; then
+        if [[ $drive == *nvme* ]]; then
+            mkfs.fat -F32 "${drive}p1" # Format EFI partition
+            mkswap "${drive}p2"         # Format swap partition
+            mkfs.ext4 "${drive}p3"      # Format root partition
+
+            # Mount partitions
+            mount "${drive}p3" /mnt
+            mkdir -p /mnt/boot/EFI
+            mount "${drive}p1" /mnt/boot/EFI
+            swapon "${drive}p2"
+        elif [[ $drive == *sd* ]]; then
+            mkfs.fat -F32 "${drive}1" # Format EFI partition
+            mkswap "${drive}2"         # Format swap partition
+            mkfs.ext4 "${drive}3"      # Format root partition
+
+            # Mount partitions
+            mount "${drive}3" /mnt
+            mkdir -p /mnt/boot/EFI
+            mount "${drive}1" /mnt/boot/EFI
+            swapon "${drive}2"
+        else
+            echo "Error: Something went wrong with UEFI install..."
+            return 1
+        fi
+    else
+        if [[ $drive == *sd* ]]; then
+            mkfs.fat -F32 "${drive}1" # Format EFI partition
+            mkswap "${drive}2"         # Format swap partition
+            mkfs.ext4 "${drive}3"      # Format root partition
+
+            # Mount partitions
+            mount "${drive}3" /mnt
+            mkdir -p /mnt/boot/EFI
+            mount "${drive}1" /mnt/boot/EFI
+            swapon "${drive}2"
+        else
+            echo "Error: Something happened with BIOS install..."
+            return 1
+        fi 
+    fi
+}
+
+# Installer Function
+installer() {
+    echo "--------------------------------"
+    print_info "Installing Arch base..."
+    sleep 2
+    pacstrap /mnt base base-devel linux linux-firmware vi vim nano dhcpcd iwd >/dev/null 2>&1
+    sleep 2
+    genfstab -U /mnt >> /mnt/etc/fstab
+}
+
+# Last Minute Setup Function
+lastMinSetup() {
+    echo "--------------------------------------"
+    print_info "Finishing last minute setup..."
+    echo 0 > /proc/sys/kernel/hung_task_timeout_secs
+    mv enable_parallel.txt /mnt
+    curl -O $GITHUB/main/postInstall.sh >/dev/null 2>&1
+    chmod +x hArchPost
+    mv hArchPost /mnt
+    echo  "----------------------------"
+    print_info "Time to enter CHROOT"
+    print_info "The final script has been installed and moved to your new root directory"
+    print_info "Run these commands to finish setup"
+    echo "------------------------------------------"
+    echo ${NEWLINE}
+    echo ${NEWLINE}
+    echo -e "[1]${GREEN} arch-chroot /mnt${RESET}"
+    echo -e "[2]${GREEN} ./hArchPost${RESET}"
+    echo ${NEWLINE}
+}
+
+# Main Installation Flow
+main() {
+    greet
+    sleep 1
+    checkNet
+    sleep 1
+    echo "----------------------------"
+    print_info "Gathering drives..."
+    lsblk #TODO: Add something more robust
+    print_info "Time to select the drive you want too partition..."
+    print_info "If you want to manually partition you may, just type exit after mounting /mnt"
+    print_info "Otherwise, automated partition will create the standard BIOS/UEFI partitions"
+    echo ${NEWLINE}
+    echo "--------------------------------------"
+    read -p "Manually partition [y/N]: " manual
+
+    if [[ $manual == 'y' || $manual == 'Y' ]]; then
+        cfdisk "$drive"
+        print_info "Entering BASH shell. Create filesystem and mount /mnt. Type exit once finished."
+        bash
+    else
+        echo "----------------------------------------------"
+        read -p "Enter Drive to prepare (e.g., /dev/sda): " drive
+        echo "----------------------------"
+        print_info "Modifying $drive..."
+        sleep 3
+
+        partition_drive "$drive"
+    fi
+
+    installer || log_error "Base system installation failed. Exiting."
+
+    sleep_and_clear
+    
+    lastMinSetup || log_warning "Last minute setup encountered issues."
+
+    log_success "hArch Installation completed successfully!"
+
+}
+
+# Start the script
+main
---
+++ postInstall.sh
@@ -0,0 +1,279 @@
+#!/usr/bin/env bash
+
+# ---------------------------------------------------------------
+# Arskell - Post install - Downloadeded after initial script     |
+# ---------------------------------------------------------------
+# Author    : Binary-Brawler                                     |
+# Github    : github.com/Binary-Brawler                          |
+# LinkedIn  : linkedin.com/in/brandon-walker-0b0542116/          |
+# Version   : 1.0.2                                              |
+# Intent    : Simplistic Linux box for Haskell Development       |
+# ---------------------------------------------------------------
+
+# Constants
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+WHITE='\033[0;37m'
+YELLOW='\033[0;33m'
+PURPLE='\033[0;35m'
+RESET='\033[0m'
+NEWLINE=$'\n'
+GITHUB='https://raw.githubusercontent.com/Binary-Brawler/Arskell'
+
+# Logging
+log_warning() { echo -e "[${YELLOW}WARNING${RESET}] $1"; }
+log_error() { echo -e "[${RED}ERROR${RESET}] $1"; }
+print_info() { echo -e "[${GREEN}INFO${RESET}] $1${NEWLINE}"; }
+log_success() {
+    local num_quotes=${#motivational_quotes[@]}
+    local random_index=$((RANDOM % num_quotes))
+    local random_quote="${programming_quotes[$random_index]}"
+    echo -e "${GREEN}[SUCCESS]${RESET} $1"
+    echo "Motivational Quote: $random_quote"
+}
+
+# Array of motivational quotes
+programming_quotes=(
+    "The only way to do great work is to love what you do. - Steve Jobs"
+    "First, solve the problem. Then, write the code. - John Johnson"
+    "The best way to predict the future is to invent it. - Alan Kay"
+    "Code is like humor. When you have to explain it, it’s bad. - Cory House"
+    "Programming isn't about what you know; it's about what you can figure out. - Chris Pine"
+    "Simplicity is the soul of efficiency. - Austin Freeman"
+    "Don’t worry if it doesn’t work right. If everything did, you’d be out of a job. - Mosher’s Law of Software Engineering"
+)
+
+# Repeats turned into functions ;) 
+sleep_and_clear() {
+    sleep 3
+    clear
+}
+
+# Are we enabling parallel downloads again...
+function pDownload() {
+    echo "--------------------------------"
+    # Inside your arch-chroot script
+    enable_parallel=$(cat /enable_parallel.txt)
+
+    if [ "$enable_parallel" = "true" ]; then
+        # Enable parallel downloads
+        sed -i '/#ParallelDownloads/s/^#//g' /etc/pacman.conf
+        print_info "Parallel Downloads Enabled."
+    else
+        print_info "Parallel Downloads Disabled."
+    fi
+}
+
+# Basic Packages
+installer() {
+    echo "-------------------------------------------------------"
+    print_info "What would you like your system name set too..."
+    read -p "Enter Hostname: " hostname
+    echo $hostname > /etc/hostname
+    sleep_and_clear
+    echo  "------------------------------------"
+    print_info "Installing useful packages..." 
+    pacman -S dkms linux-headers mlocate cmake neofetch net-tools dnsutils fish --noconfirm >/dev/null 2>&1
+    hwclock --systohc
+}
+
+# Setup Desktop Env
+desktopEnv() {
+    echo "------------------------"
+    print_info "Setting up DE..."
+    pacman -S  mate mate-extra lightdm lightdm-gtk-greeter xorg xorg-server xorg-apps xorg-xinit --noconfirm >/dev/null 2>&1
+    systemctl enable lightdm >/dev/null 2>&1
+    sleep 3
+    curl -O $GITHUB/main/linux-vs-windows.jpg >/dev/null 2>&1
+    mv /linux-vs-windows.jpg /usr/share/backgrounds/mate/desktop/linux-vs-windows.jpg
+    curl -O $GITHUB/main/MateConfig >/dev/null 2>&1
+    sleep 2
+    dconf reset -f /org/mate/
+    sleep 1
+    
+    # Attempt to load MateConfig into dconf.. Pain getting this working
+    load_dconf() {
+        dconf load /org/mate/ < MateConfig
+        return $?
+    }
+
+    if ! load_dconf; then
+        log_error "---------------------------------------------------------"
+        echo "Error: Failed to load dconf settings."
+        echo "Try manually running - (dconf load /org/mate/ < MateConfig)"
+    else
+        echo "-------------------------------------"
+        echo "Successfully loaded dconf settings."
+    fi
+}
+
+# Setup Developer Env
+devSetup() {
+    echo "----------------------------------------------------------"
+    echo "Setting up a coding environment... This may take a while"
+    echo "----------------------------------------------------------"
+
+    # Determine users to configure .vimrc
+    users=$(ls /home)
+
+    # Download and set up vimrc
+    curl -O $GITHUB/main/vimrc_bundle_conf >/dev/null 2>&1
+    for user in $users; do
+        mv vimrc_bundle_conf /home/$user/.vimrc
+    done
+
+    # Install packages
+    pacman -S jdk-openjdk python-pip rustup go nodejs npm python3 git code neovim gimp audacity wireshark-qt vlc btop virtualbox postman docker pycharm-community-edition intellij-idea-community-edition --noconfirm >/dev/null 2>&1
+
+    # Install Haskell tools
+    echo "Installing Haskell tools..."
+    userHome=$(ls /home | head -n 1)  # Assuming only one user for simplicity
+    mkdir -p /home/$userHome/AUR
+    cd /home/$userHome/AUR
+    git clone https://aur.archlinux.org/ghcup-hs-bin.git
+    cd ghcup-hs-bin
+    
+    # Run makepkg as the user without requiring input
+    sudo -u $userHome makepkg -si --noconfirm
+
+    echo "Setup complete!"
+}
+
+# Nvidia function to handle nvidia driver installation... WIP
+# vidDriver will handle for now...
+handle_nvidia() {
+    NVD='NVIDIA'
+    card=$(lspci -vmm | grep VGA -A6)
+    if [[ $card == *"$NVD"* ]]; then
+        print_info "Nvidia graphics detected. Applying configuration..."
+        # Add Nvidia-specific configurations here
+        # This will replace vidDriver() NVD case
+    fi
+}
+
+# Basic Video Driver setup
+vidDriver() {
+    echo "----------------------------------"
+    print_info "Gathering Graphics info..."
+    sleep 2
+    str=$(lspci -vmm | grep VGA -A6)
+    AMD='AMD'
+    NVD='NVIDIA'
+    case $str in
+        *"$AMD"*)
+            print_info "Installing AMD Drivers..."
+            echo "------------------------------------"
+            pacman -S xf86-video-ati xf86-video-amdgpu mesa --noconfirm >/dev/null
+            ;;
+        *"$NVD"*)
+            print_info "Installing NVIDIA Drivers..."
+            echo "-------------------------------------"
+            pacman -S nvidia nvidia-settings nvidia-utils glxinfo nvtop --noconfirm >/dev/null
+            curl -O $GITHUB/main/nvidia.hook >/dev/null
+            curl -O $GITHUB/main/20-nvidia.conf >/dev/null
+            mv nvidia.hook /etc/pacman.d/hooks/
+            mv 20-nvidia.conf /etc/X11/xorg.conf.d/
+            bash -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+            echo "-------------------------------------------------------------"
+            print_info "Attempting too force composition..."
+            print_info "[!] Usually fixes screen tearing w/ Nvidia drivers...
+            bash -c "nvidia-settings --assign CurrentMetaMode=\"$(nvidia-settings -q CurrentMetaMode -t | sed 's/"/\\"/g; s/}/, ForceCompositionPipeline = On}/')\"
+            # FIXME
+            # Add changes to mkinitcpio.conf - WIP
+            # If issues, try:
+            # xrandr --setprovideroutputsource modesetting NVIDIA-0
+            # xrandr --auto
+            ;;
+        *)
+            print_info "Unable to determine Graphics info.. Installing default drivers"
+            echo "----------------------------------------------------------------------"
+            pacman -S xf86-video-fbdev --noconfirm
+    esac
+}
+
+# Setup local/root user accounts
+userInfo() {
+    echo "--------------------------------"
+    print_info "Setting Root password..."
+    passwd
+    sleep_and_clear
+    echo "-------------------------------"
+    read -p "Enter Username: " username
+    useradd -mg users -G wheel,power,storage -s /usr/bin/fish $username
+    echo '%wheel ALL=(ALL:ALL) ALL' >> /etc/sudoers.d/wheel_group
+    chmod 440 /etc/sudoers.d/wheel_group
+    print_info "Password for user: $username"
+    passwd $username
+}
+
+# Bootloader - eventually will be automated
+booter() {
+    echo "-----------------------------"
+    print_info "Setting Bootloader..."
+    drives=$(lsblk -f)
+    echo -e "$drives${NEWLINE}"
+    echo -e "[${YELLOW}SYNTAX${RESET}] If BIOS, attach bootloader to disk: ex- /dev/sda"
+    echo -e "[${YELLOW}SYNTAX${RESET}] If UEFI, attach bootloader to partition: ex- /dev/nvme0n1p1"
+    echo "-----------------------------------------------------------------------------------------------------"
+    read -p "Enter Drive/Partition to install Bootloader [Example: /dev/nvme0n1p1]: " drive
+    sleep_and_clear
+    if [[ -d "/sys/firmware/efi" ]]; then
+        echo "-------------------------------------"
+        print_info "Installing UEFI Bootloader..."
+        pacman -S efibootmgr grub dosfstools mtools os-prober --noconfirm >/dev/null
+        grub-install --target=x86_64-efi --bootloader-id=HARCH_UEFI --efi-directory=/boot/EFI --recheck
+        grub-mkconfig -o /boot/grub/grub.cfg
+        mkinitcpio -p linux
+    else
+        echo "-------------------------------------"
+        print_info "Installing BIOS Bootloader..."
+        pacman -S grub --noconfirm >/dev/null
+        grub-install --target=i386-pc $drive --recheck
+        grub-mkconfig -o /boot/grub/grub.cfg
+        mkinitcpio -p linux
+    fi
+}
+
+complete() {
+    sleep_and_clear
+    echo "-------------------------------------------------------------------------------------------------"
+    neofetch
+    echo "-------------------------------------------------------------------------------------------------"
+    log_success "hArch has been successfully installed on your system"
+    print_info "--------------------------------------------------------------------"
+    print_info "Hack the Universe $username"
+    log_warning "A reboot should now take place"
+    print_info "Run the following commands to reboot properly:"
+    log_warning "1: exit"
+    log_warning "2: umount -a"
+    log_warning "3: reboot"
+    exit
+}
+
+# oneFuncToRuleThemAll
+oneFuncToRuleThemAll() {
+    pDownload
+    sleep_and_clear
+
+    installer
+    sleep_and_clear
+
+    userInfo
+    sleep_and_clear
+
+    vidDriver
+    sleep_and_clear
+
+    booter
+    sleep_and_clear
+
+    desktopEnv
+    sleep_and_clear
+
+    devSetup
+    sleep_and_clear
+
+    complete
+}
+    
+oneFuncToRuleThemAll
